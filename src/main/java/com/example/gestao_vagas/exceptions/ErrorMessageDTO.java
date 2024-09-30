478b73f5eb8fd1ccad67cdd120331b0d0e641f21
joschonarth <joschonarth@gmail.com>
2024-09-30 19:44:02 -0300

    Update Projeto de Gestão de Vagas com Java e Spring Boot

---
+++ src/main/java/com/example/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,15 @@
+package com.example.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+
+    private String message;
+    private String field;
+
+
+    
+}
