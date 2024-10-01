37b9b7d81c4781cba1f6029a3a450674341b4542
joschonarth <joschonarth@gmail.com>
2024-10-01 08:48:58 -0300

    Update Projeto de Gestão de Vagas com Java e Spring Boot

--- src/main/java/com/example/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/example/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,11 +1,13 @@
 package com.example.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.gestao_vagas.modules.candidate.CandidateEntity;
+import com.example.gestao_vagas.modules.candidate.CandidateRepository;
 
 import jakarta.validation.Valid;
 
@@ -13,9 +15,11 @@ import jakarta.validation.Valid;
 @RequestMapping("/candidate")
 public class CandidateController {
     
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
     }
 }
