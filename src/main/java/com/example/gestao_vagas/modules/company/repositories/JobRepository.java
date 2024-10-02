15045bd13ada67b30d41ba1a3c975c1952268ed8
joschonarth <joschonarth@gmail.com>
2024-10-02 10:25:50 -0300

    Update Projeto de Gestão de Vagas com Java e Spring Boot

---
+++ src/main/java/com/example/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -0,0 +1,10 @@
+package com.example.gestao_vagas.modules.company.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.gestao_vagas.modules.company.entities.JobEntity;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {    
+}
