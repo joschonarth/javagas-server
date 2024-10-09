123f46ced6f4d2af9c16ba9d9bd353d5ba6f759c
joschonarth <joschonarth@gmail.com>
2024-10-09 15:49:14 -0300

    Update Projeto de Gestão de Vagas com Java e Spring Boot

---
+++ src/main/java/com/example/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,12 @@
+package com.example.gestao_vagas.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+    private String description;
+    private String benefits;
+    private String level;
+    
+}
