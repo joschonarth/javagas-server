9ea4139eb719c084d63ecf8e6dd327c7e4975296
joschonarth <joschonarth@gmail.com>
2024-10-11 16:12:02 -0300

    Update Projeto de Gestão de Vagas com Java e Spring Boot

---
+++ src/main/java/com/example/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,12 @@
+package com.example.gestao_vagas.modules.candidate.useCases;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthCandidateUseCase {
+
+    public void execute() {
+        
+    }
+    
+}
