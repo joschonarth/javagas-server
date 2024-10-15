ced57cbd10f99f5556401ddedd5df0b2717b1d28
joschonarth <joschonarth@gmail.com>
2024-10-15 00:02:52 -0300

    Update Swagger Documentation

--- src/main/java/com/example/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/example/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,18 +25,25 @@ public class CandidateEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Schema(example = "João Otávio", requiredMode = RequiredMode.REQUIRED, description = "Nome do candidato")
     private String name;
 
     @NotBlank()
     @Pattern(regexp = "\\S+", message = "O campo [username] não pode conter espaços")
+    @Schema(example = "joschonarth", requiredMode = RequiredMode.REQUIRED, description = "Username do candidato")
     private String username;
 
     @Email(message = "O campo [email] deve conter um e-mail válido")
+    @Schema(example = "joschonarth@gmail.com", requiredMode = RequiredMode.REQUIRED, description = "Email do candidato")
     private String email;
 
     @Length(min = 6, max = 100, message = "A senha deve ter no mínimo 6 caracteres")
+    @Schema(example = "admin@123", minLength = 6, maxLength = 100, requiredMode = RequiredMode.REQUIRED, description = "Senha do candidato")
     private String password;
+
+    @Schema(example = "Desenvolvedor Java", requiredMode = RequiredMode.REQUIRED, description = "Breve descrição do candidato")
     private String description;
+
     private String curriculum;
 
     @CreationTimestamp
--- src/main/java/com/example/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/example/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -33,6 +33,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações dos candidatos")
 public class CandidateController {
     
     @Autowired
@@ -45,6 +46,15 @@ public class CandidateController {
     private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping("/")
+    @Operation(summary = "Cadastro de candidato", description = "Essa função é responsável por cadastrar um candidato.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(
+                schema = @Schema(implementation = CandidateEntity.class)
+            )
+        }),
+        @ApiResponse(responseCode = "400", description = "Usuário já cadastrado")
+    })
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
             var result = this.createCandidateUseCase.execute(candidateEntity);
@@ -56,7 +66,6 @@ public class CandidateController {
 
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações dos candidatos")
     @Operation(summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato.")
     @ApiResponses({
         @ApiResponse(responseCode = "200", content = {
@@ -79,7 +88,6 @@ public class CandidateController {
 
     @GetMapping("/job")
     @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações dos candidatos")
     @Operation(summary = "Listagem de vagas disponíveis para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro.")
     @ApiResponses({
         @ApiResponse(responseCode = "200", content = {
