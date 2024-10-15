7ddacd285c3e3ff8b8f25a8ad845c71f5a227757
joschonarth <joschonarth@gmail.com>
2024-10-14 22:08:59 -0300

    Update Swagger Documentation

--- src/main/java/com/example/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/com/example/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -1,10 +1,13 @@
 package com.example.gestao_vagas.modules.company.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.gestao_vagas.modules.company.entities.JobEntity;
 
-public interface JobRepository extends JpaRepository<JobEntity, UUID> {    
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+    
+    List<JobEntity> findByDescriptionContaining(String filter);
 }
