d5f025354363878cee6efd4c700abeaaa405c9eb
joschonarth <joschonarth@gmail.com>
2024-10-17 16:18:50 -0300

    Adding unit tests

---
+++ src/test/java/com/example/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -0,0 +1,41 @@
+package com.example.gestao_vagas.modules.candidate.useCases;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.gestao_vagas.exceptions.UserNotFoundException;
+import com.example.gestao_vagas.modules.candidate.CandidateRepository;
+import com.example.gestao_vagas.modules.company.repositories.JobRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyJobCandidateUseCaseTest {
+ 
+    @InjectMocks
+    private ApplyJobCandidateUseCase applyJobCandidateUseCase;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Mock
+    private JobRepository jobRepository;
+
+    @Test
+    @DisplayName("Should not be able to apply job when candidate not found")
+    public void shouldNotApplyJobWhenCandidateNotFound() {
+        try {
+            applyJobCandidateUseCase.execute(null, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(UserNotFoundException.class);
+
+        }
+         
+
+    }
+    
+}
