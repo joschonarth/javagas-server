840f777888fcb1ac14ced0e6585027fc1996b5b6
joschonarth <joschonarth@gmail.com>
2024-10-18 19:50:11 -0300

    Adding test to validate job application for non-existent jobs

--- src/test/java/com/example/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/com/example/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,10 @@
 package com.example.gestao_vagas.modules.candidate.useCases;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.example.gestao_vagas.exceptions.JobNotFoundException;
 import com.example.gestao_vagas.exceptions.UserNotFoundException;
+import com.example.gestao_vagas.modules.candidate.CandidateEntity;
 import com.example.gestao_vagas.modules.candidate.CandidateRepository;
 import com.example.gestao_vagas.modules.company.repositories.JobRepository;
 
@@ -32,10 +38,23 @@ public class ApplyJobCandidateUseCaseTest {
             applyJobCandidateUseCase.execute(null, null);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(UserNotFoundException.class);
-
         }
-         
+    }
 
+    @Test
+    public void shouldNotApplyJobWhenJobNotFound() {
+        var idCandidate = UUID.randomUUID();
+        
+        var candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+
+        when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+
+        try {
+            applyJobCandidateUseCase.execute(idCandidate, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
+        }
     }
     
 }
