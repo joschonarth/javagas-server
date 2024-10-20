dcf4939c28af71e66a1176c2c561f98271da32be
joschonarth <joschonarth@gmail.com>
2024-10-19 22:07:36 -0300

    Refactor

--- src/main/java/com/example/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
+++ src/main/java/com/example/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -3,9 +3,9 @@ package com.example.gestao_vagas.modules.candidate.useCases;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.example.gestao_vagas.exceptions.UserNotFoundException;
 import com.example.gestao_vagas.modules.candidate.CandidateRepository;
 import com.example.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 
@@ -18,7 +18,7 @@ public class ProfileCandidateUseCase {
     public ProfileCandidateResponseDTO execute(UUID idCandidate) {
         var candidate = this.candidateRepository.findById(idCandidate)
             .orElseThrow(() -> {
-                throw new UsernameNotFoundException("User not found");
+                throw new UserNotFoundException();
             });
         
         var candidateDTO = ProfileCandidateResponseDTO.builder()
